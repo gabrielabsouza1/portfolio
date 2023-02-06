95a789e3428543f80da820dc30d2ab521fb31311
Gabi <gabigabrielabs@gmail.com>
2023-02-06 08:03:05 -0300

    navbar and sections

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as data from '../../public/data/navlinks.json';
+import styles from './style.module.scss';
+const linksString = JSON.stringify(data);
+const links = JSON.parse(linksString).links;
+
+type NavLink = {
+    label: string;
+    href: string;
+};
+
+const Links: React.FC<{ links: NavLink[] }> = ({ links }) => {
+    return (
+        <div className={styles['links-container']}>
+            {links.map((link: NavLink) => {
+                return (
+                    <div key={link.href} className={styles['link']}>
+                        <a href={link.href}>
+                            {link.label}
+                        </a>
+                    </div>
+                )
+            })}
+        </div>
+    )
+};
+
+const Navbar: React.FC<{}> = () => {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles['logo-container']}>
+                <a href={'/'}><img width={70} src="/img/logo2.png" className='img-fluid' alt="" /></a>
+            </div>
+            <Links links={links} />
+        </nav>
+    )
+}
+
+export default Navbar;

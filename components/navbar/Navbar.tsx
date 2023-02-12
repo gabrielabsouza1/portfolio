bd204e02114063c61fe210b6a70651867ddd0a2c
Gabi <gabigabrielabs@gmail.com>
2023-02-12 23:51:14 +1100

    responsive navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as data from '../../public/data/navlinks.json';
 import styles from './style.module.scss';
+import Lottie from 'react-lottie';
+import menuData from '../../public/data/menu.json';
 const linksString = JSON.stringify(data);
 const links = JSON.parse(linksString).links;
 
@@ -10,24 +12,41 @@ type NavLink = {
 };
 
 const Links: React.FC<{ links: NavLink[] }> = ({ links }) => {
+    const [open, setOpen] = useState(false);
+
+    const buttonHandler = (event: any) => {
+        event.preventDefault();
+        setOpen(!open);
+    };
+
     return (
-        <div className={styles['links-container']}>
-            {links.map((link: NavLink) => {
-                return (
-                    <div key={link.href} className={styles['link']}>
-                        <a href={link.href}>
-                            {link.label}
-                        </a>
-                    </div>
-                )
-            })}
+        <div className={styles.links_container}>
+            <div className={open ? `${styles.nav_icon} ${styles.open}` : `${styles.nav_icon}`} onClick={buttonHandler}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className='position-relative'>
+                <div className={styles.list_links} style={open ? { maxHeight: 240 + 'px', visibility: 'visible'} : { maxHeight: 0, visibility: 'hidden' }}>
+                    {links.map((link: NavLink) => {
+                        return (
+                            <div key={link.href} className={styles.link}>
+                                <a href={link.href}>
+                                    {link.label}
+                                </a>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 };
 
 const Navbar: React.FC<{}> = () => {
+
     return (
-        <nav className={styles.navbar} style={{zIndex: 10}}>
+        <nav className={styles.navbar} style={{ zIndex: 10 }}>
             <div className={styles['logo-container']}>
                 <a href={'/'}><img width={70} src="/img/logo2.png" className='img-fluid' alt="" /></a>
             </div>
